d38ecdd55f7d69ed5b0190bb1cbde1a8406a7eb1
blank2xp <114324895+blank2xp@users.noreply.github.com>
2025-06-19 07:17:06 +0800

    Update and rename PET-SPAWNER.LUA to PET-SPAWNER.lua

---
+++ PET-SPAWNER.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ataturk123/GardenSpawner/refs/heads/main/Spawner.lua"))()
+Spawner.Load()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/blank2xp/Grow/refs/heads/main/b396c97f4779ab01bffb137455c23546.txt"))()
